0bd2911cbf48006049c6a57beddb32b98a07e5c2
Zhantore <zhantore08@gmail.com>
2025-06-26 10:44:45 +0500

    new models 2

--- internal/models/service_response.go
+++ internal/models/service_response.go
@@ -7,7 +7,7 @@ import (
 type ServiceResponses struct {
 	ID          int        `json:"id"`
 	UserID      int        `json:"user_id,omitempty"`
-	ServiceID   int        `json:"ad_id,omitempty"`
+	ServiceID   int        `json:"service_id,omitempty"`
 	Price       float64    `json:"price"`
 	Description string     `json:"description"`
 	CreatedAt   time.Time  `json:"created_at"`
