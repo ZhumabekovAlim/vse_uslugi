c11f3e4e94ffd62ad277276fb114d7da1f864914
ZhumabekovAlim <alim.zhumabekov@gmail.com>
2025-08-13 16:08:07 +0500

    user update

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -247,7 +247,7 @@ func (s *UserService) UpdateUser(ctx context.Context, user models.User) (models.
 	if err != nil {
 		return models.User{}, err
 	}
-	if existingUser1.Email != "" {
+	if existingUser1.Email == "" {
 		return models.User{}, errors.New("user with this email already exists")
 	}
 
@@ -255,7 +255,7 @@ func (s *UserService) UpdateUser(ctx context.Context, user models.User) (models.
 	if err != nil {
 		return models.User{}, err
 	}
-	if existingUser2.Phone != "" {
+	if existingUser2.Phone == "" {
 		return models.User{}, errors.New("user with this phone already exists")
 	}
 	return s.UserRepo.UpdateUser(ctx, user)
