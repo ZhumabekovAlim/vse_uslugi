4c45ab9229bf02dc1b830936aa05059f82b63e97
ZhumabekovAlim <alim.zhumabekov@gmail.com>
2025-11-10 06:17:44 +0500

    taxi

--- cmd/middleware.go
+++ cmd/middleware.go
@@ -27,6 +27,7 @@ func makeResponseJSON(next http.Handler) http.Handler {
 
 func (app *application) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Received request:", r)
 		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
 		next.ServeHTTP(w, r)
 	})
