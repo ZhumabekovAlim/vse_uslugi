9ff6c519decae5bdbbbe26022e12259572d944d7
ZhumabekovAlim <alim.zhumabekov@gmail.com>
2025-11-10 18:13:24 +0500

    taxi

--- internal/models/ad_reviews.go
+++ internal/models/ad_reviews.go
@@ -10,11 +10,9 @@ type AdReviews struct {
 	AdID           int        `json:"ad_id,omitempty"`
 	Rating         float64    `json:"rating"`
 	Review         string     `json:"review"`
-	UserName       string     `json:"user_name"`
-	UserSurname    string     `json:"user_surname"`
-
-	UserAvatarPath *string    `json:"user_avatar_path"`
-
-	CreatedAt      time.Time  `json:"created_at"`
+	UserName       *string    `json:"user_name,omitempty"`
+	UserSurname    *string    `json:"user_surname,omitempty"`
+	UserAvatarPath *string    `json:"user_avatar_path,omitempty"`
+	CreatedAt      *time.Time `json:"created_at,omitempty"`
 	UpdatedAt      *time.Time `json:"updated_at,omitempty"`
 }
