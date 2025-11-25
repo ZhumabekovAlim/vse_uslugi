0c81ca4493eeaf7f27a1a9af8a7268d82a5ddae0
ZhumabekovAlim <alim.zhumabekov@gmail.com>
2025-11-26 03:57:59 +0500

    main func fix

--- internal/repositories/rent_ad_repository.go
+++ internal/repositories/rent_ad_repository.go
@@ -24,7 +24,7 @@ type RentAdRepository struct {
 func (r *RentAdRepository) CreateRentAd(ctx context.Context, rent models.RentAd) (models.RentAd, error) {
 	query := `
     INSERT INTO rent_ad (name, address, price, price_to, user_id, images, videos, category_id, subcategory_id, description, avg_rating, top, negotiable, hide_phone, liked, status, rent_type, deposit, latitude, longitude, created_at)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 `
 
 	// Сохраняем images как JSON
