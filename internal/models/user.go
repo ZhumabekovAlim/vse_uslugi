ec7a34e3832b06c2e568178298db09cee685e9bb
Alim Zhumabekov <145676784+ZhumabekovAlim@users.noreply.github.com>
2025-12-14 01:33:21 +0500

    Add role-aware sign-in by phone

--- internal/models/user.go
+++ internal/models/user.go
@@ -65,6 +65,7 @@ type SignInRequest struct {
 	Login    string `json:"login"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+	Role     string `json:"role"`
 }
 
 type UpdatePasswordRequest struct {
