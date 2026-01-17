936e1a150ab6c41c6c9bca40f3bf79d96962d55d
ZhumabekovAlim <alim.zhumabekov@gmail.com>
2026-01-17 17:37:10 +0500

    IAP config

--- internal/services/apple_iap_service.go
+++ internal/services/apple_iap_service.go
@@ -212,6 +212,7 @@ func (s *AppleIAPService) fetchSignedTransaction(ctx context.Context, transactio
 	if err != nil {
 		return "", err
 	}
+	log.Printf("[APPLE IAP] jwt len=%d head=%.20s", len(token), token)
 
 	base := appStoreProdBase
 	if env == "sandbox" {
